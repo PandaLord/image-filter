0fe1b2feb56a39da885c99fb557a58178618fd74
PandaLord <330213905@qq.com>
2023-10-11 09:21:07 +0800

    impl mutilple features but havent passed test yet

--- src/engine.rs
+++ src/engine.rs
@@ -1,17 +1,27 @@
 use photon_rs::PhotonImage;
-use crate::{function::transform::*, prelude::{TransformAction, TransformImpl}};
+use crate::{function::transform::*, prelude::{TransformAction, TransformImpl, MultipleImpl, MultipleAction}};
 pub trait Engine {
-    fn apply(&mut self, actions: &[TransformAction]);
+    fn transform(&mut self, actions: &[TransformAction]);
+    fn multiple(&mut self, img2: &PhotonImage, actions: &[MultipleAction]);
 }
 
 pub struct Photon(pub PhotonImage);
 
 impl Engine for Photon {
-    fn apply(&mut self, actions: &[TransformAction]) {
+    fn transform(&mut self, actions: &[TransformAction]) {
         for action in actions.iter() {
             action.transform(&mut self.0);
         }
     }
+
+    fn multiple(&mut self, img2: &PhotonImage, actions: &[MultipleAction]) {
+        for action in actions.iter() {
+            action.multiple(&mut self.0, img2);
+        }
+    }
+
+    
+
 }
 
 impl Photon {
--- src/function/mod.rs
+++ src/function/mod.rs
@@ -11,6 +11,7 @@ mod mono_effect;
 mod text_apply;
 mod watermark;
 pub mod transform;
+pub mod multiple;
 use transform::*;
 
 use filters::Filter;
@@ -35,14 +36,30 @@ pub enum TransformAction {
 }
 
 
+#[enum_dispatch]
+pub trait MultipleImpl {
+    fn multiple(&self, img: &mut PhotonImage, img2: &PhotonImage);
+}
+
+
+#[enum_dispatch(MultipleImpl)]
+pub enum MultipleAction {
+    Blend(multiple::Blend),
+    Fade(multiple::Fade),
+    ApplyGradient(multiple::ApplyGradient),
+    CreateGradient(multiple::CreateGradient),
+    ReplaceBg(multiple::ReplaceBg),
+    Watermark(multiple::Watermark)
+}
+
+
 
 
 #[cfg(test)]
 mod test {
     use super::*;
     use crate::engine::{Engine, Photon};
-    use photon_rs::native::save_image;
-    use transform::*;
+    use photon_rs::{native::save_image, Rgb};
     #[test]
     fn test_transform() {
         let img = photon_rs::native::open_image("pic/test.jpg").unwrap();
@@ -58,7 +75,37 @@ mod test {
             TransformAction::Flipv(Filpv),
             TransformAction::Rotate(Rotate { angle: 90 }),
         ];
-        handler.apply(actions.as_ref());
+        handler.transform(actions.as_ref());
         assert!(save_image(handler.0, "pic/test_after.jpg").is_ok());
     }
+
+    #[test]
+    fn multiple_test() {
+        let img = photon_rs::native::open_image("pic/test_after.jpg").unwrap();
+        let img2 = photon_rs::native::open_image("pic/test.jpg").unwrap();
+        let mut handler = Photon::new(img);
+        let actions = vec![
+            MultipleAction::Blend(multiple::Blend {
+                mode: multiple::BlendMode::Darken,
+            }),
+            MultipleAction::Fade(multiple::Fade {
+                start_x: 0,
+                end_x: 100,
+                start_y: 0,
+                end_y: 100,
+            }),
+            MultipleAction::ApplyGradient(multiple::ApplyGradient),
+            MultipleAction::CreateGradient(multiple::CreateGradient {
+                width: 100,
+                height: 100,
+            }),
+            MultipleAction::ReplaceBg(multiple::ReplaceBg {
+                rgb: Rgb::new(255,255,255)
+            }),
+            MultipleAction::Watermark(multiple::Watermark { x: 0, y: 0 }),
+        ];
+        handler.multiple(&img2, actions.as_ref());
+        assert!(save_image(handler.0, "pic/test_multiple.jpg").is_ok());
+    }
+
 }
